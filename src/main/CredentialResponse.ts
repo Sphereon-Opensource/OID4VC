a0801e7cc90ed8ec7beef5aefcc1d014c465b7a8
sksadjad <86593551+sksadjad@users.noreply.github.com>
2022-10-10 16:14:11 +0200

    feature: CredentialResponseError added

--- src/main/CredentialResponse.ts
+++ src/main/CredentialResponse.ts
@@ -6,3 +6,18 @@ export interface CredentialResponse {
   credential: W3CVerifiableCredential;
   format: CredentialFormat;
 }
+
+export interface CredentialResponseError {
+  error: CredentialResponseErrorCode;
+  error_description?: string;
+  error_uri?: string;
+}
+
+export enum CredentialResponseErrorCode {
+  INVALID_OR_MISSING_PROOF = 'invalid_or_missing_proof',
+  INVALID_REQUEST = 'invalid_request',
+  INVALID_TOKEN = 'invalid_token',
+  UNSUPPORTED_TYPE = 'unsupported_type',
+  UNSUPPORTED_FORMAT = 'unsupported_format',
+  INVALID_CREDENTIAL = 'invalid_credential',
+}
