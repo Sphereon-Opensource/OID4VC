7a71c180aa998fb2799ac0cca2056f74b662bbb7
sksadjad <86593551+sksadjad@users.noreply.github.com>
2022-10-11 15:00:07 +0200

    changed the structure of project, added client and client builder

--- src/main/types/VCIssuance.types.ts
+++ src/main/types/VCIssuance.types.ts
@@ -1,21 +1,41 @@
-import { CredentialRequest } from '../CredentialRequest';
+import { W3CVerifiableCredential } from '@sphereon/ssi-types';
+import { ClaimFormat } from '@sphereon/ssi-types/src/types/vc';
 
-export enum CredentialFormat {
-  JWT_VC = 'jwt_vc',
-  LDP_VC = 'ldp_vc',
-}
-
-export enum CredentialType {
-  OPEN_BADGE_CREDENTIAL = 'https://imsglobal.github.io/openbadges-specification/ob_v3p0.html#OpenBadgeCredential',
+export interface CredentialRequest {
+  //TODO: handling list is out of scope for now
+  type: string | string[];
+  //TODO: handling list is out of scope for now
+  format: ClaimFormat | ClaimFormat[];
+  proof: ProofOfPossession;
 }
 
 export enum ProofType {
   JWT = 'jwt',
 }
 
-export interface ProofOfPossesion {
+export interface CredentialResponse {
+  credential: W3CVerifiableCredential;
+  format: ClaimFormat;
+}
+
+export interface CredentialResponseError {
+  error: CredentialResponseErrorCode;
+  error_description?: string;
+  error_uri?: string;
+}
+
+export enum CredentialResponseErrorCode {
+  INVALID_OR_MISSING_PROOF = 'invalid_or_missing_proof',
+  INVALID_REQUEST = 'invalid_request',
+  INVALID_TOKEN = 'invalid_token',
+  UNSUPPORTED_TYPE = 'unsupported_type',
+  UNSUPPORTED_FORMAT = 'unsupported_format',
+  INVALID_CREDENTIAL = 'invalid_credential',
+}
+
+export interface ProofOfPossession {
   proof_type: ProofType;
-  jws: string;
+  jwt: string;
   [x: string]: any;
 }
 
