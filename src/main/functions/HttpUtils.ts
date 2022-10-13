40b7e99aa14aad98335dce4b750a418c111596f9
sksadjad <86593551+sksadjad@users.noreply.github.com>
2022-10-13 11:55:59 +0200

    Update src/main/functions/HttpUtils.ts
    Co-authored-by: Niels Klomp <nklomp@users.noreply.github.com>

--- src/main/functions/HttpUtils.ts
+++ src/main/functions/HttpUtils.ts
@@ -26,7 +26,7 @@ export async function postWithBearerToken(
 
 export function isValidURL(url: string) {
   const urlPattern = new RegExp(
-    '^(https?:\\/\\/)?' + // validate protocol
+    '^(https:\\/\\/)?' + // validate protocol
       '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // validate domain name
       '((\\d{1,3}\\.){3}\\d{1,3}))' + // validate OR ip (v4) address
       '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // validate port and path
