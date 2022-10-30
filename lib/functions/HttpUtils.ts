cfb080265b34df6934dc579aeb06f09b9a267f7d
nklomp <nklomp@sphereon.com>
2022-10-30 18:06:47 +0100

    fix: allow : in path for velocity network

--- lib/functions/HttpUtils.ts
+++ lib/functions/HttpUtils.ts
@@ -115,7 +115,7 @@ export function isValidURL(url: string): boolean {
     '^(https:\\/\\/)?' + // validate protocol
       '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // validate domain name
       '((\\d{1,3}\\.){3}\\d{1,3}))' + // validate OR ip (v4) address
-      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // validate port and path
+      '(\\:\\d+)?(\\/[-a-z\\d%_.~+:]*)*' + // validate port and path
       '(\\?[;&a-z\\d%_.~+=-]*)?' + // validate query string
       '(\\#[-a-z\\d_]*)?$',
     'i'
