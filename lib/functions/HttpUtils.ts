792f1d7526d8a369c03d4606b7bcaf74518b22cb
nklomp <nklomp@sphereon.com>
2023-01-12 01:10:39 +0100

    fix: localhost not handled in isValidUrl and http(s) scheme not being required. closes #23

--- lib/functions/HttpUtils.ts
+++ lib/functions/HttpUtils.ts
@@ -94,8 +94,9 @@ const openIdFetch = async <T>(
 
 export const isValidURL = (url: string): boolean => {
   const urlPattern = new RegExp(
-    '^(https?:\\/\\/)?' + // validate protocol
+    '^(https?:\\/\\/)' + // validate protocol
       '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // validate domain name
+      '((localhost))|' + // validate OR localhost
       '((\\d{1,3}\\.){3}\\d{1,3}))' + // validate OR ip (v4) address
       '(\\:\\d+)?(\\/[-a-z\\d%_.~+:]*)*' + // validate port and path
       '(\\?[;&a-z\\d%_.~+=-]*)?' + // validate query string
