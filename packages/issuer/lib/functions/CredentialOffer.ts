bd1569c8b8b1404d90db822ecc8925a2485e46ba
sksadjad <86593551+sksadjad@users.noreply.github.com>
2023-04-06 11:16:12 +0200

    feat: added (issuer) state to options for createCredentialOfferDeeplink
    Signed-off-by: sksadjad <86593551+sksadjad@users.noreply.github.com>

--- packages/issuer/lib/functions/CredentialOffer.ts
+++ packages/issuer/lib/functions/CredentialOffer.ts
@@ -1,7 +1,7 @@
 import { CredentialFormatEnum, CredentialSupported, encodeJsonAsURI, IssuerMetadata, TokenErrorResponse } from '@sphereon/openid4vci-common'
 import { v4 as uuidv4 } from 'uuid'
 
-export function createCredentialOfferDeeplink(preAuthorizedCode: string, issuerMetadata: IssuerMetadata): string {
+export function createCredentialOfferDeeplink(preAuthorizedCode: string, issuerMetadata: IssuerMetadata, opts?: { state?: string }): string {
   // openid-credential-offer://credential_offer=%7B%22credential_issuer%22:%22https://credential-issuer.example.com
   // %22,%22credentials%22:%5B%7B%22format%22:%22jwt_vc_json%22,%22types%22:%5B%22VerifiableCr
   // edential%22,%22UniversityDegreeCredential%22%5D%7D%5D,%22issuer_state%22:%22eyJhbGciOiJSU0Et...
@@ -20,7 +20,7 @@ export function createCredentialOfferDeeplink(preAuthorizedCode: string, issuerM
       format: issuerMetadata.credentials_supported.map((cs) => cs.format),
       types: types,
       //fixme: @nklomp I've placed this here for now, but later we need to have the concept of sessions and in there we have to keep track of the id
-      issuer_state: uuidv4(),
+      issuer_state: opts && opts.state ? opts.state : uuidv4(),
     },
     grants: {
       authorization_code: preAuthorizedCode,
