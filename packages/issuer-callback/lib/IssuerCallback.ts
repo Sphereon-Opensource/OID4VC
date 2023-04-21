99c9d5e792d93aec5509647a80236569f55dcc2d
Maikel Maas <mmaas@sphereon.com>
2023-04-21 16:42:57 +0200

    VDX-189 Implement V11 Credential Response in OpenID4VCI Issuer and REST
       - Implemented the callback function;
       - Implemented unit tests;
       - Refactored;

--- packages/issuer-callback/lib/IssuerCallback.ts
+++ packages/issuer-callback/lib/IssuerCallback.ts
@@ -1,15 +1,25 @@
+import * as didKeyDriver from '@digitalcredentials/did-method-key'
 import { Ed25519Signature2020 } from '@digitalcredentials/ed25519-signature-2020'
 import { Ed25519VerificationKey2020 } from '@digitalcredentials/ed25519-verification-key-2020'
-import vc, { defaultDocumentLoader } from '@digitalcredentials/vc'
+import { securityLoader } from '@digitalcredentials/security-document-loader'
+import vc from '@digitalcredentials/vc'
 import { CredentialRequest } from '@sphereon/openid4vci-common'
 import { ICredential, W3CVerifiableCredential } from '@sphereon/ssi-types'
+
+export const generateDid = async () => {
+  const didKD = didKeyDriver.driver()
+  const { didDocument, keyPairs, methodFor } = await didKD.generate()
+  return { didDocument, keyPairs, methodFor }
+}
 
 export const credentialCallback = async (opts: {
   credentialRequest?: CredentialRequest
   credential?: ICredential
 }): Promise<W3CVerifiableCredential> => {
+  const documentLoader = securityLoader().build()
   const keyPair = await Ed25519VerificationKey2020.generate()
   const suite = new Ed25519Signature2020({ key: keyPair })
+  suite.verificationMethod = (await generateDid()).didDocument.verificationMethod[0].id
   const credential = opts.credential
-  return await vc.issue({ credential, suite, defaultDocumentLoader })
+  return await vc.issue({ credential, suite, documentLoader })
 }
--- packages/issuer-callback/lib/__tests__/issuerCallback.spec.ts
+++ packages/issuer-callback/lib/__tests__/issuerCallback.spec.ts
@@ -1,16 +1,38 @@
+import * as didMethodKey from '@digitalcredentials/did-method-key'
 import { ICredential } from '@sphereon/ssi-types'
 
 import { credentialCallback } from '../IssuerCallback'
 
 describe('issuerCallback', () => {
+  let didKey: string
+
+  beforeAll(async () => {
+    const didKeyDriver = didMethodKey.driver()
+    const { didDocument } = await didKeyDriver.generate()
+    didKey = didDocument.id
+  })
+
   it('should issue a VC', async () => {
     const credential: ICredential = {
-      '@context': [],
+      '@context': ['https://www.w3.org/2018/credentials/v1'],
       type: ['VerifiableCredential'],
-      issuer: 'did:key',
+      issuer: didKey,
       credentialSubject: {},
       issuanceDate: new Date().toISOString(),
     }
-    await expect(credentialCallback({ credential })).resolves.toEqual({})
+    await expect(credentialCallback({ credential })).resolves.toEqual({
+      '@context': ['https://www.w3.org/2018/credentials/v1', 'https://w3id.org/security/suites/ed25519-2020/v1'],
+      credentialSubject: {},
+      issuanceDate: expect.any(String),
+      issuer: expect.stringContaining('did:key:'),
+      proof: {
+        created: expect.any(String),
+        proofPurpose: 'assertionMethod',
+        proofValue: expect.any(String),
+        type: 'Ed25519Signature2020',
+        verificationMethod: expect.any(String),
+      },
+      type: ['VerifiableCredential'],
+    })
   })
 })
