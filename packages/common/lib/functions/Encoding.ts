71dbeac25c5497ee13c90373ecc3cad61e14829e
Zoë Maas <zmaas@sphereon.com>
2024-05-07 13:41:46 +0200

    refactor: Refactored JSDoc

--- packages/common/lib/functions/Encoding.ts
+++ packages/common/lib/functions/Encoding.ts
@@ -1,9 +1,9 @@
 import { BAD_PARAMS, DecodeURIAsJsonOpts, EncodeJsonAsURIOpts, JsonURIMode, OpenId4VCIVersion, SearchValue } from '../types';
 
 /**
- * @function encodeJsonAsURI encodes a Json object into a URI
- * @param json object
- * @param opts:
+ * @type {(json: {[s:string]: never} | ArrayLike<never> | string | object, opts?: EncodeJsonAsURIOpts)} encodes a Json object into a URI
+ * @param { {[s:string]: never} | ArrayLike<never> | string | object } json
+ * @param {EncodeJsonAsURIOpts} [opts] Option to encode json as uri
  *          - urlTypeProperties: a list of properties of which the value is a URL
  *          - arrayTypeProperties: a list of properties which are an array
  */
@@ -82,8 +82,8 @@ export function convertJsonToURI(
 
 /**
  * @type {(uri: string, opts?: DecodeURIAsJsonOpts): unknown} convertURIToJsonObject converts an URI into a Json object decoding its properties
- * @param uri string
- * @param opts
+ * @param {string} uri
+ * @param {DecodeURIAsJsonOpts} [opts]
  *          - requiredProperties: the required properties
  *          - arrayTypeProperties: properties that can show up more that once
  * @returns JSON object
