230ad37b06cc6d7c91f4dd212463f274b5c6a6af
Niels Klomp <nklomp@sphereon.com>
2024-06-12 00:01:56 +0200

    chore: update deps

--- packages/common/lib/index.ts
+++ packages/common/lib/index.ts
@@ -1,6 +1,6 @@
 import { Loggers, LogMethod } from '@sphereon/ssi-types';
 
-export const VCI_LOGGERS = Loggers.default();
+export const VCI_LOGGERS = Loggers.DEFAULT;
 export const VCI_LOG_COMMON = VCI_LOGGERS.options('sphereon:oid4vci:common', { methods: [LogMethod.EVENT, LogMethod.DEBUG_PKG] }).get(
   'sphereon:oid4vci:common',
 );
