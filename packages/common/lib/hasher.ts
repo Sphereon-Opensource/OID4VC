735a2774653b6e5e08c594f55bb2d65deeca6805
sanderPostma <spostma@sphereon.com>
2024-08-28 20:33:51 +0200

    chore: sanitizedAlgorithm

--- packages/common/lib/hasher.ts
+++ packages/common/lib/hasher.ts
@@ -5,12 +5,13 @@ const supportedAlgorithms = ['sha256', 'sha384', 'sha512'] as const;
 type SupportedAlgorithms = (typeof supportedAlgorithms)[number];
 
 export const defaultHasher: Hasher = (data, algorithm) => {
-  if (!supportedAlgorithms.includes(algorithm as SupportedAlgorithms)) {
+  const sanitizedAlgorithm = algorithm.toLowerCase().replace(/[-_]/g, '')
+  if (!supportedAlgorithms.includes(sanitizedAlgorithm as SupportedAlgorithms)) {
     throw new Error(`Unsupported hashing algorithm ${algorithm}`);
   }
 
   return new Uint8Array(
-    sha(algorithm as SupportedAlgorithms)
+    sha(sanitizedAlgorithm as SupportedAlgorithms)
       .update(data)
       .digest(),
   );
