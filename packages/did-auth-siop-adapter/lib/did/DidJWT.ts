23483b6a77adf8f6a7c715da34b830fdeae72614
sanderPostma <spostma@sphereon.com>
2024-08-30 12:47:09 +0200

    chore: NP fixes

--- packages/did-auth-siop-adapter/lib/did/DidJWT.ts
+++ packages/did-auth-siop-adapter/lib/did/DidJWT.ts
@@ -81,10 +81,16 @@ export async function createDidJWT(
 
 export async function signIDTokenPayload(payload: IDTokenPayload, signature: InternalSignature | ExternalSignature | SuppliedSignature) {
   if (isInternalSignature(signature)) {
+    if(!signature.kid) {
+      return Promise.reject(Error('missing kid from signature'))
+    }
     return signDidJwtInternal(payload, payload.issuer, signature.hexPrivateKey, signature.alg, signature.kid, signature.customJwtSigner)
   } else if (isExternalSignature(signature)) {
     return signDidJwtExternal(payload, signature.signatureUri, signature.authZToken, signature.alg, signature.kid)
   } else if (isSuppliedSignature(signature)) {
+    if(!signature.kid) {
+      return Promise.reject(Error('missing kid from signature'))
+    }
     return signDidJwtSupplied(payload, payload.issuer, signature.signature, signature.alg, signature.kid)
   } else {
     throw new Error(
@@ -108,10 +114,16 @@ export async function signRequestObjectPayload(payload: RequestObjectPayload, si
     payload.sub = signature.did
   }
   if (isInternalSignature(signature)) {
+    if(!signature.kid) {
+      return Promise.reject(Error('missing kid from signature'))
+    }
     return signDidJwtInternal(payload, issuer, signature.hexPrivateKey, signature.alg, signature.kid, signature.customJwtSigner)
   } else if (isExternalSignature(signature)) {
     return signDidJwtExternal(payload, signature.signatureUri, signature.authZToken, signature.alg, signature.kid)
   } else if (isSuppliedSignature(signature)) {
+    if(!signature.kid) {
+      return Promise.reject(Error('missing kid from signature'))
+    }
     return signDidJwtSupplied(payload, issuer, signature.signature, signature.alg, signature.kid)
   } else {
     throw new Error(
@@ -154,7 +166,7 @@ async function signDidJwtExternal(
     payload,
     expiresIn: DEFAULT_EXPIRATION_TIME,
     alg,
-    selfIssued: payload.iss.includes(ResponseIss.SELF_ISSUED_V2) ? payload.iss : undefined,
+    selfIssued: payload.iss && payload.iss.includes(ResponseIss.SELF_ISSUED_V2) ? payload.iss : undefined,
     kid,
   }
 
