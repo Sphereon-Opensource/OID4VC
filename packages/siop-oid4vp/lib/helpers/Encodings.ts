97904e2998e3be6cff205f680a43ec90b78d2493
A.G.J. Cate <brummos@gmail.com>
2025-09-12 14:18:11 +0200

    fix: added back missing encoding functions

--- packages/siop-oid4vp/lib/helpers/Encodings.ts
+++ packages/siop-oid4vp/lib/helpers/Encodings.ts
@@ -71,3 +71,20 @@ export function encodeJsonAsURI(json: Record<string, unknown>, _opts?: { arraysW
   }
   return results.join('&')
 }
+
+export function base64ToHexString(input: string, encoding?: 'base64url' | 'base64'): string {
+  return u8a.toString(u8a.fromString(input, encoding ?? 'base64url'), 'base16')
+}
+
+export function fromBase64(base64: string): string {
+  return base64.replace(/=/g, '').replace(/\+/g, '-').replace(/\//g, '_')
+}
+
+export function base64urlEncodeBuffer(buf: { toString: (arg0: 'base64') => string }): string {
+  return fromBase64(buf.toString('base64'))
+}
+
+export function base64urlToString(base64url: string): string {
+  const uint8array = u8a.fromString(base64url, 'base64url')
+  return u8a.toString(uint8array, 'ascii')
+}
