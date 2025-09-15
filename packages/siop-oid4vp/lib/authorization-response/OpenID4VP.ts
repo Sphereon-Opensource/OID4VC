53ffde29a41e545bb9f1eb12f98e8957b4355977
sanderPostma <spostma@sphereon.com>
2025-09-15 18:02:55 +0200

    chore: Validate incoming DcqlQuery and return DcqlPresentationResult in VerifiedOpenID4VPSubmission

--- packages/siop-oid4vp/lib/authorization-response/OpenID4VP.ts
+++ packages/siop-oid4vp/lib/authorization-response/OpenID4VP.ts
@@ -50,6 +50,7 @@ export const verifyPresentations = async (
   verifyOpts: VerifyAuthorizationResponseOpts,
 ): Promise<{ dcql: VerifiedOpenID4VPSubmission }> => {
   const dcqlQuery = DcqlQuery.parse(verifyOpts.dcqlQuery ?? JSON.parse(authorizationResponse?.authorizationRequest.payload.dcql_query))
+  DcqlQuery.validate(dcqlQuery)
   const dcqlPresentation = extractDcqlPresentationFromDcqlVpToken(authorizationResponse.payload.vp_token as string, { hasher: verifyOpts.hasher })
 
   const wrappedPresentations = Object.values(dcqlPresentation)
@@ -59,7 +60,7 @@ export const verifyPresentations = async (
       ),
     )
 
-    await Dcql.assertValidDcqlPresentationResult(authorizationResponse.payload.vp_token as string, dcqlQuery, { hasher: verifyOpts.hasher })
+    const dcqlPresentationResult = await Dcql.assertValidDcqlPresentationResult(authorizationResponse.payload.vp_token as string, dcqlQuery, { hasher: verifyOpts.hasher })
 
     if (verifiedPresentations.some((verified) => !verified)) {
       const message = verifiedPresentations
@@ -95,7 +96,7 @@ export const verifyPresentations = async (
     }
   }
 
-  return { dcql: { nonce, presentation: dcqlPresentation, dcqlQuery } }
+  return { dcql: { nonce, presentation: dcqlPresentation, dcqlQuery , dcqlPresentationResult} }
 }
 
 export const extractDcqlPresentationFromDcqlVpToken = (
--- packages/siop-oid4vp/lib/types/SIOP.types.ts
+++ packages/siop-oid4vp/lib/types/SIOP.types.ts
@@ -10,7 +10,7 @@ import {
   W3CVerifiablePresentation,
   WrappedVerifiablePresentation,
 } from '@sphereon/ssi-types'
-import { DcqlQuery } from 'dcql'
+import { DcqlPresentationResult, DcqlQuery } from 'dcql'
 import {
   AuthorizationRequest,
   CreateAuthorizationRequestOpts,
@@ -522,6 +522,7 @@ export interface VerifiedIDToken {
 export interface VerifiedOpenID4VPSubmission {
   dcqlQuery: DcqlQuery
   presentation: { [credentialQueryId: string]: WrappedVerifiablePresentation }
+  dcqlPresentationResult?: DcqlPresentationResult
   nonce?: string
 }
 
