729baea0ea7d76c9a7fdd08e34e177c1744a4f1a
sanderPostma <spostma@sphereon.com>
2025-09-22 18:25:31 +0200

    chore: revert findValidDcqlQuery after syncup

--- packages/siop-oid4vp/lib/authorization-response/Dcql.ts
+++ packages/siop-oid4vp/lib/authorization-response/Dcql.ts
@@ -26,17 +26,17 @@ import {AuthorizationRequestPayload, SupportedVersion} from '../types'
 
 export class Dcql {
   static findValidDcqlQuery = async (authorizationRequestPayload: AuthorizationRequestPayload, version?: SupportedVersion): Promise<DcqlQuery | undefined> => {
-    const dcqlQueryStrings: string[] = extractDataFromPath(authorizationRequestPayload ?? {}, '$..dcql_query').map((d) => d.value)
+    const dcqlQuery: DcqlQuery.Input[] = extractDataFromPath(authorizationRequestPayload ?? {}, '$..dcql_query').map((d) => d.value)
 
-    if (dcqlQueryStrings.length === 0) {
+    if (dcqlQuery.length === 0) {
       return undefined
     }
 
-    if (dcqlQueryStrings.length > 1) {
+    if (dcqlQuery.length > 1) {
       throw new Error('Found multiple dcql_query in vp_token. Only one is allowed')
     }
 
-    const parsedDcqlQuery = DcqlQuery.parse(JSON.parse(dcqlQueryStrings[0]))
+    const parsedDcqlQuery = DcqlQuery.parse(dcqlQuery[0])
 
     if (version === SupportedVersion.OID4VP_v1) {
       const hasMeta = parsedDcqlQuery.credentials
